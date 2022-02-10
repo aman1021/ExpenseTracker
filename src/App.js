7de73bff36c088282c6238e0adf0618efb1fa810
Aman Chandel <amaniiitu32@gmail.com>
2022-02-10 22:26:33 +0530

    passed data via props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import ExpenseItem from "./Components/ExpenseItem";
+//import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses"
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Car Insuarance",
+      amount: 450,
+      date: new Date(2022, 2, 10),
+    },
+    {
+      id: 2,
+      title: "Groceries",
+      amount: 6000,
+      date: new Date(2022, 2, 11),
+    },
+    {
+      id: 3,
+      title: "car cleaning",
+      amount: 4500,
+      date: new Date(2022, 2, 8),
+    },
+    {
+      id: 4,
+      title: "Books",
+      amount: 1000,
+      date: new Date(2022, 2, 6),
+    },
+  ];
   return (
+
     <div>
       <h1>Lets make our project.</h1>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
-function ExpenseItem(){
-    const expenseTitle = 'Expense-Title'
+function ExpenseItem(props){
+    
     return(
         <div className = "expense-item">
-            <div>Date</div>
+            <ExpenseDate date = {props.date}></ExpenseDate>
             <div className = "expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">$ {props.amount}</div>
             </div>
         </div>
-    )
+    );
 }
 
 //Exporting the Component
